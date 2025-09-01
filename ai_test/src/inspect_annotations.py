fa3acdf22cab26832b1cd836aeb9e29115ac2b8a
Julius Harnack <jharnack@uni-bremen.de>
2025-09-01 14:45:37 +0200

    Delete ai_Test and move it to a Test Branch

--- ai_test/src/inspect_annotations.py
+++
@@ -1,7 +0,0 @@
-import mne
-from pathlib import Path
-from mne.datasets import sample
-
-p = Path(sample.data_path(verbose=False)) / "MEG" / "sample" / "sample_audvis_raw.fif"
-raw = mne.io.read_raw_fif(str(p), preload=False, verbose=False)
-print(sorted(set(a["description"] for a in raw.annotations)))
